8de847b23c57db47d4a79fc85f5a2d6b2b358ec6
MAX ROMA <MAX.ROMA01@ESTUDIANT.UPF.EDU>
2026-05-11 20:05:33 +0200

    Add enemy cut direction arrows

---
+++ Assets/Scripts/EnemyCutDirection.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyCutDirection : MonoBehaviour
+{
+    public enum CutDirection
+    {
+        Horizontal,
+        Vertical,
+        DiagonalSlash,
+        DiagonalBackslash
+    }
+
+    public CutDirection requiredCutDirection;
+
+    [SerializeField] private float arrowAppearDelay = 1f;
+
+    private Transform arrowTransform;
+    private float arrowAngle;
+
+    private void Start()
+    {
+        arrowTransform = transform.Find("CutDirectionArrow");
+
+        if (arrowTransform == null)
+        {
+            Debug.LogWarning("CutDirectionArrow was not found on " + gameObject.name);
+            return;
+        }
+
+        // Hide arrow immediately when enemy spawns
+        arrowTransform.gameObject.SetActive(false);
+
+        requiredCutDirection = (CutDirection)Random.Range(0, 4);
+
+        SetArrowAngle();
+        UpdateArrowVisual();
+
+        Debug.Log(gameObject.name + " cut direction is " + requiredCutDirection);
+
+        // Show arrow after delay
+        StartCoroutine(ShowArrowAfterDelay());
+    }
+
+    private IEnumerator ShowArrowAfterDelay()
+    {
+        yield return new WaitForSeconds(arrowAppearDelay);
+
+        if (arrowTransform != null)
+        {
+            arrowTransform.gameObject.SetActive(true);
+        }
+    }
+
+    private void SetArrowAngle()
+    {
+        switch (requiredCutDirection)
+        {
+            case CutDirection.Horizontal:
+                arrowAngle = 0f;
+                break;
+
+            case CutDirection.Vertical:
+                arrowAngle = 90f;
+                break;
+
+            case CutDirection.DiagonalSlash:
+                arrowAngle = 45f;
+                break;
+
+            case CutDirection.DiagonalBackslash:
+                arrowAngle = -45f;
+                break;
+        }
+    }
+
+    private void UpdateArrowVisual()
+    {
+        if (arrowTransform == null)
+        {
+            Debug.LogWarning("CutDirectionArrow was not found on " + gameObject.name);
+            return;
+        }
+
+        // Rotate around Y for your top-down/floor direction
+        arrowTransform.localRotation = Quaternion.Euler(0f, arrowAngle, 0f);
+    }
+}
